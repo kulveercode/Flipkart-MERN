76f0785364ce6059c71f480875d7832770606b01
Kulveer Singh <kulveer.coder@gmail.com>
2024-01-28 00:11:37 +0530

    carousel

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import Navbar from './Navbar'
+import Banner from './Banner'
+import { Box, styled } from '@mui/material'
+
+const Componet = styled(Box)`
+    margin: 10px;
+    background: #f2f2f2;
+`
 
 function Home() {
   return (
-    <div>
+    <>
       <Navbar />
-    </div>
+      <Componet>
+      <Banner />
+      </Componet>
+    </>
   )
 }
 
